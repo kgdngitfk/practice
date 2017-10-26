5866acb07fe00f7cf1b78727fa20457dd03da467
kgdngitfk <ngitfk@qq.com>
2017-10-26 23:10:47 +0800

    ?

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -4,5 +4,19 @@
 public class Start {
     public static void main(String[] args) {
         System.out.println( Start.class.getName());
+        for (int i = 0; i <3 ; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    int i=0;
+                    while (true){
+                        if(true) i++;
+                        else ;
+                    }
+                }
+            }).start();
+        }
+
+
     }
 }
